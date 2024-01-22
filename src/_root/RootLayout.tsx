f4643f41d7bd87f6742eb6baabedbc9172c201cb
Johann Monteiro <monteiro.johann@gmail.com>
2024-01-22 10:58:04 -0300

    topbar done

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+
+import Topbar from '@/components/shared/Topbar'
+import Bottombar from '@/components/shared/bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+
+
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className='w-full mid:flex'>
+      <Topbar />
+      <LeftSidebar />
+
+      <section className='flex flex-1 h-full'>
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
   )
 }
 
-export default RootLayout
+export default RootLayout;
