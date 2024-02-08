f3e2e2a598fdd31f36b690f1b8c23d5e1d336073
Johann Monteiro <monteiro.johann@gmail.com>
2024-02-08 12:08:09 -0300

    post creation working, sending data to appwrite ok

--- src/lib/react-query/querysAndMutations.ts
+++ src/lib/react-query/querysAndMutations.ts
@@ -25,3 +25,15 @@ export const useSignOutAccount = () => {
     mutationFn: signOutAccount
   });
 };
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
