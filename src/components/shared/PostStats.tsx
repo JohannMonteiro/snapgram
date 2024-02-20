0747686bb3c69b0887d373314cc1bee90fb61b6b
Johann Monteiro <monteiro.johann@gmail.com>
2024-02-20 09:46:01 -0300

    Finish edit post, still can't test it because posting isn't working

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -7,7 +7,8 @@ import {
 } from "@/lib/react-query/querysAndMutations";
 import { checkIsLiked } from "@/lib/utils";
 import { Models } from "appwrite";
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
+import Loader from "./Loader";
 
 type PostStatsProps = {
   post: Models.Document;
@@ -20,13 +21,23 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const [isSaved, setIsSaved] = useState(false);
 
   const { mutate: likePost } = useLikePost();
-  const { mutate: savePost } = useSavePost();
-  const { mutate: deleteSavedPost } = useDeleteSavedPost();
+  const { mutate: savePost, isPending: isSavingPost } = useSavePost();
+  const { mutate: deleteSavedPost, isPending: isDeletingSaved } = useDeleteSavedPost();
 
   const { data: currentUser } = useGetCurrentUser();
 
+  const savedPostRecord = currentUser?.save.find(
+    (record: Models.Document) => record.post.$id === post.$id
+  );
+
+  // double !! changes the boolean to its opposite
+
+  useEffect(() => { setIsSaved(!!savedPostRecord)},[currentUser]);
+  
+
   const handleLikePost = (e: React.MouseEvent) => {
     e.stopPropagation();
+
 
     let newLikes = [...likes];
 
@@ -45,13 +56,9 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
   const handleSavePost = (e: React.MouseEvent) => {
     e.stopPropagation();
 
-    const savePostRecord = currentUser?.save.find(
-      (record: Models.Document) => record.$id === post.$id
-    );
-
-    if (savePostRecord) {
+    if (savedPostRecord) {
       setIsSaved(false);
-      deleteSavedPost(savePostRecord.$id);
+      deleteSavedPost(savedPostRecord.$id);
     } else {
       savePost({ postId: post.$id, userId });
       setIsSaved(true);
@@ -71,18 +78,18 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
           height={20}
           onClick={handleLikePost}
           className="cursor-pointer"
-        />
+        /> 
         <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
       <div className="flex gap-2">
-        <img
+      {isSavingPost || isDeletingSaved ? <Loader/> : <img
           src={`${isSaved}?"/assets/icons/saved.svg":"/assets/icons/save.svg"`}
           alt="like"
           width={20}
           height={20}
           onClick={handleSavePost}
           className="cursor-pointer"
-        />
+        />}
       </div>
     </div>
   );
