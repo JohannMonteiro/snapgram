516104f319783bac3d082ba032a473d4d4ba2c85
Johann Monteiro <monteiro.johann@gmail.com>
2024-02-23 10:37:46 -0300

    fix  fileUploader

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,9 +1,11 @@
 import { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
+
 import { Button } from "../ui/button";
+import { convertFileToUrl } from "@/lib/utils";
 
 type FileUploaderProps = {
-  fieldChange: (FILES: file[]) => void;
+  fieldChange: (files: File[]) => void;
   mediaUrl: string;
 };
 
@@ -15,7 +17,7 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
     (acceptedFiles: FileWithPath[]) => {
       setFile(acceptedFiles);
       fieldChange(acceptedFiles);
-      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+      setFileUrl(convertFileToUrl(acceptedFiles[0]));
     },
     [file]
   );
